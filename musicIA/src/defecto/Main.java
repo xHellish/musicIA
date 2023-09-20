0c0d9cc977e05de1a7d656e1c2e7e5a0522735bb
Ben Farz <benfarz.bf25@gmail.com>
2023-09-20 11:16:41 -0600

    Final

--- musicIA/src/defecto/Main.java
+++ musicIA/src/defecto/Main.java
@@ -6,10 +6,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 		LetraIA letra1 = new LetraIA("Frase");
+		
 		CancionIA cancion1 = new CancionIA(letra1, "Pop");
-		
-		
+		CancionIA cancion2 = new CancionIA(letra1, "Rock");
+		CancionIA cancion3 = new CancionIA(letra1, "Bachata");
 		
 	}
 }
--- musicIA/src/musicIA/CancionIA.java
+++ musicIA/src/musicIA/CancionIA.java
@@ -2,12 +2,28 @@ package musicIA;
 
 public class CancionIA {
 	
+	String _letra;
 	String archivoMP3;
+	boolean reproduciendo;
+	
 	
 	public CancionIA (LetraIA letra, String genero) {
 		
+		_letra = letra.letrafinal;
+		reproduciendo = true;
 		archivoMP3 = "Reproduciendo canción con letra " + letra.letrafinal + " y género " + genero;
 		System.out.println(archivoMP3);
+		
+		
+		
 	}
-
+	
+	public void agregarGenero (String nuevoGenero) {
+		
+		if (reproduciendo == false) {
+			archivoMP3 = "Reproduciendo canción con letra " + _letra + " y género " + nuevoGenero;
+		}else {
+			System.out.println("Guardado en cola canción de género "+ nuevoGenero);
+		}	
+	}
 }
